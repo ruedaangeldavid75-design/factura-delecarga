75ebe41c1275961648a7e2fee34ef4e5bce9789a
ruedaangeldavid75-design <ruedaangeldavid75@gmail.com>
2026-04-29 14:35:05 -0500

    cambios_29_04_2026

--- Codigo_factura/service-worker.js
+++ Codigo_factura/service-worker.js
@@ -1,30 +1,50 @@
-const CACHE_NAME = 'delecarga-v1';
-
+const CACHE_NAME = 'delecarga-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './estilo.css',
-  './script.js',
-  './img/imagen_logo_delecarga.png'
+    './',
+    './index.html',
+    './estilo.css',
+    './script.js',
+    './manifest.json',
+    './img/imagen_logo_delecarga.png',
+    './img/fecha_servicio.png',
+    './img/imagen_circular_recortada_ubicacion_dos.png',
+    './img/imagen_circular_recortada_ubicacion_tres.png',
+    './img/carro_delecarga.png',
+    './img/descripcion_del_servicio.png',
+    './img/imagen_circular_recortada_dinero_uno.png',
+    './img/imagen_circular_recortada_dinero_dos.png',
+    './img/imagen_circular_recortada_dineros.png',
+    './img/imagen_circular_recortada_estrechomanos.png',
+    './img/telefono.png',
+    './img/logo whattaspts.png',
+    './img/imagen_circular_recortada_proteccion.png'
 ];
 
-// Instalar
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
 });
 
-// Activar
 self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim())
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
-// Fetch (offline)
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
